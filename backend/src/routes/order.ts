d3f883de5591dd78fbf7d43a2dea8ddb5162656a
Dmitry Kostylev <smileyfvce@yandex.ru>
2026-06-05 00:08:56 +0300

    fix: project

--- backend/src/routes/order.ts
+++ backend/src/routes/order.ts
@@ -16,11 +16,7 @@ import { Role } from '../models/user'
 const orderRouter = Router()
 const csrfProtection = csrf({ cookie: true });
 
-<<<<<<< HEAD
 orderRouter.post('/', auth, csrfProtection, validateOrderBody, createOrder)
-=======
-orderRouter.post('/', auth, validateOrderBody, createOrder)
->>>>>>> review
 orderRouter.get('/all', auth, roleGuardMiddleware(Role.Admin), getOrders)
 orderRouter.get('/all/me', auth, getOrdersCurrentUser)
 orderRouter.get(
